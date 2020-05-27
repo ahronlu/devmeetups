606823eddcf4a151f6352e72c05504081171418a
koftov <ahronlu@gmail.com>
2020-05-27 13:50:06 +0300

    addes signup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import { store } from './store'
 import DateFilter from './filters/date'
@@ -15,5 +16,16 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDrYfU8jmfxxYN_ONRyMmWimPSQ3w0O9Wc',
+      authDomain: 'devmeetupz.firebaseapp.com',
+      databaseURL: 'https://devmeetupz.firebaseio.com',
+      projectId: 'devmeetupz',
+      storageBucket: 'devmeetupz.appspot.com',
+      messagingSenderId: '375198881852',
+      appId: '1:375198881852:web:f8a6b6bc8b5a7bef1a8b39'
+    })
+  }
 })
